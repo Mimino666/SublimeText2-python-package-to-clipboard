4a57cc4997fb6e3d4e95c4ef474768bd9a28e4bd
Michal Danilak <mimino.coder@gmail.com>
2013-01-25 03:29:19 +0100

    First commit with working plugin.

---
+++ python_package_to_clipboard.py
@@ -0,0 +1,21 @@
+from os import path
+
+import sublime, sublime_plugin
+
+
+class PythonPackageToClipboardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filename = self.view.file_name()
+        pckg = [path.splitext(path.basename(filename))[0]]
+        while filename != path.dirname(filename):
+            filename = path.dirname(filename)
+            if not path.exists(path.join(filename, '__init__.py')):
+                break
+            pckg.append(path.basename(filename))
+
+        sublime.set_clipboard('.'.join(reversed(pckg)))
+        sublime.status_message("Copied python package")
+
+    def is_enabled(self):
+        filename = self.view.file_name()
+        return filename and path.splitext(filename)[1] == '.py'
