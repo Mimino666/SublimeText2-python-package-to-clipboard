8a54fa50dce5b6a3083e629ff5cac24d5625e521
Michal Danilak <mimino.coder@gmail.com>
2013-01-28 02:53:24 +0100

    Added settings to customize, which extensions should be considered as Python files. Also a little refactoring in code.

--- python_package_to_clipboard.py
+++ python_package_to_clipboard.py
@@ -1,26 +1,43 @@
 from os import path
+import sublime
+import sublime_plugin
 
-import sublime, sublime_plugin
+
+SETTINGS_FILE = 'Python Package to Clipboard.sublime-settings'
+settings = sublime.load_settings(SETTINGS_FILE)
 
 
 class PythonPackageToClipboardCommand(sublime_plugin.TextCommand):
+    def _base_filename(self, filename):
+        return path.splitext(path.basename(filename))[0]
+
+    def _is_package(self, pckg_path):
+        for ext in settings.get('python_extensions', []):
+            filename = path.join(pckg_path, '__init__' + ext)
+            if path.exists(filename):
+                return True
+        return False
+
     def run(self, edit):
         filename = self.view.file_name()
         pckg = []
-        if path.basename(filename) != '__init__.py':
-            pckg.append(path.splitext(path.basename(filename))[0])
-        while filename != path.dirname(filename):
-            filename = path.dirname(filename)
-            if not path.exists(path.join(filename, '__init__.py')):
+        if self._base_filename(filename) != '__init__':
+            pckg.append(self._base_filename(filename))
+
+        pckg_path = path.dirname(filename)
+        while self._is_package(pckg_path):
+            pckg.append(path.basename(pckg_path))
+            pckg_path, old_path = path.dirname(pckg_path), pckg_path
+            if pckg_path == old_path:
                 break
-            pckg.append(path.basename(filename))
 
         sublime.set_clipboard('.'.join(reversed(pckg)))
         sublime.status_message('Copied python package')
 
     def is_enabled(self):
         filename = self.view.file_name()
-        return filename and path.splitext(filename)[1] == '.py'
+        extensions = settings.get('python_extensions', [])
+        return filename and path.splitext(filename)[1] in extensions
 
     def is_visible(self):
         return self.is_enabled()
