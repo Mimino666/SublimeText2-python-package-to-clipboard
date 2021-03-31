3111b6bee2fd8f6db08d22a7f4d99680dc97a8ef
Michal Danilak <mimino.coder@gmail.com>
2021-03-31 09:16:06 +0200

    Fixed loading

--- python_path_to_clipboard.py
+++ python_path_to_clipboard.py
@@ -1,23 +1,13 @@
 from os import path
+
 import sublime
 import sublime_plugin
 
 
 SETTINGS_FILE = 'Python Path to Clipboard.sublime-settings'
-settings = sublime.load_settings(SETTINGS_FILE)
 
 
 class PythonPathToClipboardCommand(sublime_plugin.TextCommand):
-    def _base_filename(self, filename):
-        return path.splitext(path.basename(filename))[0]
-
-    def _is_package(self, pckg_path):
-        for ext in settings.get('python_extensions', ['.py']):
-            filename = path.join(pckg_path, '__init__' + ext)
-            if path.exists(filename):
-                return True
-        return False
-
     def run(self, edit):
         filename = self.view.file_name()
         pckg = []
@@ -35,9 +25,31 @@ class PythonPathToClipboardCommand(sublime_plugin.TextCommand):
         sublime.status_message('Copied python import path')
 
     def is_enabled(self):
-        filename = self.view.file_name()
-        extensions = settings.get('python_extensions', [])
-        return filename and path.splitext(filename)[1] in extensions
+        return self._is_current_python_file()
 
     def is_visible(self):
-        return self.is_enabled()
+        return self._is_current_python_file()
+
+    def _is_current_python_file(self):
+        '''True, if current file if a python file.'''
+
+        filename = self.view.file_name()
+        extensions = self._get_python_extensions()
+        return (filename and
+                extensions and
+                path.splitext(filename)[1] in extensions)
+
+    def _get_python_extensions(self, default=None):
+        settings = sublime.load_settings(SETTINGS_FILE)
+        return settings.get('python_extensions', default)
+
+    def _base_filename(self, filename):
+        return path.splitext(path.basename(filename))[0]
+
+    def _is_package(self, pckg_path):
+        for ext in self._get_python_extensions(['.py']):
+            filename = path.join(pckg_path, '__init__' + ext)
+            if path.exists(filename):
+                return True
+
+        return False
